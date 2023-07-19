042ee70ebb445e13a389c24ea9673ba86324e85f
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2023-07-19 17:44:40 +0530

    Feat : Dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,86 @@
+"use client";
+
+import {
+  ArrowRight,
+  MessageSquare,
+  ImageIcon,
+  VideoIcon,
+  Music,
+  Code,
+} from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-yellow-500",
+    bgColor: "bg-yellow-500/10",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-red-500",
+    bgColor: "bg-red-500/10",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-blue-500",
+    bgColor: "bg-blue-500/10",
+  },
+  {
+    label: "Code Snippet",
+    icon: Code,
+    href: "/code",
+    color: "text-pink-500",
+    bgColor: "bg-pink-500/10",
+  },
+];
 
 const DashboardPage = () => {
-    return(
-        <div>Dashboard (Protected)</div>
-    );
-}
+  const router = useRouter();
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the Power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI and experience the power of AI.
+        </p>
+        <div className="px-4 md:px-20 lg:px-32 space-y-4">
+          {tools.map((tool) => (
+            <Card
+              onClick={() => router.push(tool.href)}
+              key={tool.href}
+              className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer "
+            >
+              <div className="flex items-center gap-x-4">
+                <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                  <tool.icon className={cn("w-8 h-8", tool.color)} />
+                </div>
+                <div className="font-semibold">{tool.label}</div>
+              </div>
+              <ArrowRight className={cn("w-5 h-5")} />
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DashboardPage;
