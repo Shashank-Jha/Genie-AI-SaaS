563bf41253d989e32dcc457cbaf1a709d07b7485
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2023-07-19 14:18:25 +0530

    UI : Responsive Sidebar (update)

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Sidebar from "@/components/sidebar";
+import { useState, useEffect } from "react";
+
+const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
