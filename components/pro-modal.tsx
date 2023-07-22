dc1dbbf1cc359fa12013468654c3940c9a034afd
Shashank-Jha <53325972+Shashank-Jha@users.noreply.github.com>
2023-07-22 11:32:02 +0530

    fix : pro-modal loading state

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -62,7 +62,7 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const onSubscribe = async () => {
     try {
@@ -89,7 +89,10 @@ export const ProModal = () => {
           </DialogTitle>
           <DialogDescription className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
             {tools.map((tool) => (
-              <Card key={tool.label} className="p-3 border-black/5 flex items-center justify-between">
+              <Card
+                key={tool.label}
+                className="p-3 border-black/5 flex items-center justify-between"
+              >
                 <div className="flex items-center gap-x-4">
                   <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
                     <tool.icon className={cn("w-6 h-6 ", tool.color)} />
